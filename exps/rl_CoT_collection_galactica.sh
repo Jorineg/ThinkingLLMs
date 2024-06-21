783783ee8d59f4751d2dbac89ca45d7cfbfe56a7
Jorin Eggers <jorineggers@gmail.com>
2024-06-22 00:27:03 +0200

    change for 1 a100 gpu

--- exps/rl_CoT_collection_galactica.sh
+++ exps/rl_CoT_collection_galactica.sh
@@ -34,10 +34,10 @@ start_penalty_after="0"
 penalty_warmup_steps="1"
 
 ppo_epochs="2"
-mini_batch_size="10"
-eval_batch_size="18"
-batch_size="18"
-num_processes='2'
+mini_batch_size="70"
+eval_batch_size="75"
+batch_size="75"
+num_processes='1'
 
 keep_num_ckpt='0'
 n_epochs="700"
