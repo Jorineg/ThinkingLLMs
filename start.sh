ba582ce37519933cb3543e5f0f91e9a8eddb9499
Jorin Eggers <jorineggers@gmail.com>
2024-09-26 17:43:38 +0200

    always use 80gb card

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 #SBATCH --partition=gpu-test
 #SBATCH --gpus-per-node=1
 #SBATCH --ntasks-per-node=2
+#SBATCH --constraint="80gb"
 #SBATCH --output=logs/job-%j.out
 
 # Run the command inside the Python container, passing the third argument
