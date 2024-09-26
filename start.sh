1676c17d008197d6e4dc3e624dddaeee48c65cb0
Jorin Eggers <jorineggers@gmail.com>
2024-09-26 16:13:15 +0200

    minor changes

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=$1  # Job name as the first argument
-#SBATCH --partition=$2  # Partition as the second argument
+#SBATCH --job-name=test
+#SBATCH --partition=gpu-test
 #SBATCH --gpus-per-node=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --output=logs/job-%j.out
 
 # Run the command inside the Python container, passing the third argument
-apptainer run --nv python_container.sif $3
+apptainer run --nv python_container.sif bash -c ./exps/new_exps/rl_bigbench_gemma2.sh
