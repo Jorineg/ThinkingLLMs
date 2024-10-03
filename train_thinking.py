44cc501257304e5b1abfda3243936a1a2284dde8
Jorin Eggers <jorineggers@gmail.com>
2024-10-03 11:53:41 +0200

    fix policy freezing scheduler

--- train_thinking.py
+++ train_thinking.py
@@ -1312,7 +1312,7 @@ def main(args):
             # Create a list of lambda functions, one for each parameter group
             lr_lambdas = []
             for group in optimizer.param_groups:
-                if len(group['params'])>0 and group['params'][0].shape == model.v_head.summary.weight.shape:
+                if len(group['params'])>0 and (group['lr']==args["value_head_learning_rate"]):
                     lr_lambdas.append(lr_lambda_value_head)
                 else:
                     lr_lambdas.append(lr_lambda_policy)
